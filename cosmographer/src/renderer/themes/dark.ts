ede47b3dcebfc21e9d9cc15392e15e528687dabf
Dax Harris <matteovh@gmail.com>
2023-01-23 09:09:51 -0500

    add dark theme colors

---
+++ cosmographer/src/renderer/themes/dark.ts
@@ -0,0 +1,11 @@
+export const ThemeDark = {
+    background: {
+        light: "#121416",
+        dark: "#0A0A0A",
+    },
+    text: "#FFF8F0",
+    accent: {
+        light: "#FFA0FD",
+        dark: "#EC6FE4",
+    },
+};
