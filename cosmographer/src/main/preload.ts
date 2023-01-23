c7879d9a6fc29a182e263c1914fa92545399d1e9
Dax Harris <matteovh@gmail.com>
2023-01-22 23:42:40 -0500

    add general fs & dialog access

--- cosmographer/src/main/preload.ts
+++ cosmographer/src/main/preload.ts
@@ -1,27 +1,21 @@
-import { contextBridge, ipcRenderer, IpcRendererEvent } from 'electron';
-
-export type Channels = 'ipc-example';
+import { contextBridge, ipcRenderer } from "electron";
 
 const electronHandler = {
-  ipcRenderer: {
-    sendMessage(channel: Channels, args: unknown[]) {
-      ipcRenderer.send(channel, args);
+    expose: {
+        call(module: "fs" | "dialog", member: string, args?: any[]) {
+            return ipcRenderer.sendSync(
+                "cosm-call",
+                module,
+                member,
+                args ?? []
+            );
+        },
+        get(module: "fs" | "dialog", member: string) {
+            return ipcRenderer.sendSync("cosm-get", module, member);
+        },
     },
-    on(channel: Channels, func: (...args: unknown[]) => void) {
-      const subscription = (_event: IpcRendererEvent, ...args: unknown[]) =>
-        func(...args);
-      ipcRenderer.on(channel, subscription);
-
-      return () => {
-        ipcRenderer.removeListener(channel, subscription);
-      };
-    },
-    once(channel: Channels, func: (...args: unknown[]) => void) {
-      ipcRenderer.once(channel, (_event, ...args) => func(...args));
-    },
-  },
 };
 
-contextBridge.exposeInMainWorld('electron', electronHandler);
+contextBridge.exposeInMainWorld("electron", electronHandler);
 
 export type ElectronHandler = typeof electronHandler;
--- cosmographer/src/renderer/App.tsx
+++ cosmographer/src/renderer/App.tsx
@@ -1,16 +1,17 @@
-import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.scss';
-import React, { useEffect } from 'react';
+import { MemoryRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.scss";
+import React, { useEffect } from "react";
+import { readdir } from "./utils/ipc/fs";
 
 export default function App() {
-  useEffect(() => {
-    //dialog.showOpenDialog({ title: 'Test' });
-  }, []);
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<></>} />
-      </Routes>
-    </Router>
-  );
+    useEffect(() => {
+        console.log(readdir("."));
+    }, []);
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<></>} />
+            </Routes>
+        </Router>
+    );
 }
